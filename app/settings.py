fd3815df7073bd4a6c7794f84289bad4afcc85e8
Brian Jessup <brianjessup@gmail.com>
2016-07-06 10:58:19 -0700

    first draft

---
+++ app/settings.py
@@ -0,0 +1,107 @@
+# Django settings for project.
+import os.path
+import os
+
+PROJECT_DIR = os.path.dirname(__file__)
+STATIC_ROOT = '/home/giftovus/webapps/slp_static/static/'
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = [
+    '.slpsandiego.com',
+]
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = False
+
+# Set this to true to use timezone information
+USE_TZ = True
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# change MEDIA_URL
+MEDIA_URL = '/static/media/'
+
+# URL prefix for admin static files -- CSS, JavaScript and images.
+# Make sure to use a trailing slash.
+# Examples: "http://foo.com/static/admin/", "/static/admin/".
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'z)3rm!wmqq)xwm47^5ja_88vul0tmaj5_4nmz1ue(_)w@i76ez'
+
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    # Keep the cache turned off until we determine why it wasn't working correctly on bottlenose
+    #'johnny.middleware.LocalStoreClearMiddleware',
+    #'johnny.middleware.QueryCacheMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
+)
+
+ROOT_URLCONF = 'urls'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, 'templates'),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'django.contrib.humanize',
+    'django.contrib.sitemaps',
+)
+
+
+STATICFILES_DIRS = (os.path.join(PROJECT_DIR, 'compiled-files'),)
+
---
+++ app/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, url, include
+from django.views.generic import RedirectView, TemplateView
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # We give the root view the name 'auth_login', since this is what other
+    # packages such as django.contrib.auth expect the login view to be named.
+    # -> If the user is logged in, they are shown the dashboard.
+    # -> If not, they are shown the landing page.
+    url(r'^$', TemplateView.as_view(template_name='landing/slp-base.html'), name='home'),
+
+
+)
